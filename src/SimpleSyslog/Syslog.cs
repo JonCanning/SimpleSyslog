bef47f71ae61c48210ca161073b02c549fff9deb
Jon Canning <jon.canning@gmail.com>
2014-03-23 19:53:31 +0000

    More overloads

--- src/SimpleSyslog/Syslog.cs
+++ src/SimpleSyslog/Syslog.cs
@@ -23,28 +23,33 @@ namespace SimpleSyslog
       Syslog.facility = facility;
     }
 
-    public static void Log(LogLevel logLevel, string message, Type senderType, params object[] args)
+    public static void Log(LogLevel logLevel, string sender, string message, params object[] args)
     {
       message = string.Format(message, args);
-      var sender = fixedSender ?? senderType.Name.Replace(' ', '_');
+      sender = fixedSender ?? sender.Replace(' ', '_');
       Task.Factory.StartNew(() => Send(logLevel, message, sender));
     }
 
     public static void Log<T>(LogLevel logLevel, string message, params object[] args)
     {
-      Log(logLevel, message, typeof(T), args);
+      Log(logLevel, typeof(T).Name, message, args);
     }
 
     static void LogWithSender(LogLevel logLevel, string message, params object[] args)
     {
       var stackTrace = new StackFrame(2);
       var senderType = stackTrace.GetMethod().DeclaringType;
-      Log(logLevel, message, senderType);
+      Log(logLevel, senderType.Name, message, args);
     }
 
     public static void Emergency(string message, params object[] args)
     {
       LogWithSender(LogLevel.Emergency, message, args);
+    }
+
+    public static void Emergency(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Emergency, sender, message, args);
     }
 
     public static void Emergency<T>(string message, params object[] args)
@@ -57,6 +62,11 @@ namespace SimpleSyslog
       LogWithSender(LogLevel.Alert, message, args);
     }
 
+    public static void Alert(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Alert, sender, message, args);
+    }
+
     public static void Alert<T>(string message, params object[] args)
     {
       Log<T>(LogLevel.Alert, message, args);
@@ -65,6 +75,11 @@ namespace SimpleSyslog
     public static void Critical(string message, params object[] args)
     {
       LogWithSender(LogLevel.Critical, message, args);
+    }
+
+    public static void Critical(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Critical, sender, message, args);
     }
 
     public static void Critical<T>(string message, params object[] args)
@@ -77,6 +92,11 @@ namespace SimpleSyslog
       LogWithSender(LogLevel.Error, message, args);
     }
 
+    public static void Error(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Error, sender, message, args);
+    }
+
     public static void Error<T>(string message, params object[] args)
     {
       Log<T>(LogLevel.Error, message, args);
@@ -85,6 +105,11 @@ namespace SimpleSyslog
     public static void Warn(string message, params object[] args)
     {
       LogWithSender(LogLevel.Warn, message, args);
+    }
+
+    public static void Warn(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Warn, sender, message, args);
     }
 
     public static void Warn<T>(string message, params object[] args)
@@ -97,6 +122,11 @@ namespace SimpleSyslog
       LogWithSender(LogLevel.Notice, message, args);
     }
 
+    public static void Notice(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Notice, sender, message, args);
+    }
+
     public static void Notice<T>(string message, params object[] args)
     {
       Log<T>(LogLevel.Notice, message, args);
@@ -107,6 +137,11 @@ namespace SimpleSyslog
       LogWithSender(LogLevel.Info, message, args);
     }
 
+    public static void Info(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Info, sender, message, args);
+    }
+
     public static void Info<T>(string message, params object[] args)
     {
       Log<T>(LogLevel.Info, message, args);
@@ -115,6 +150,11 @@ namespace SimpleSyslog
     public static void Debug(string message, params object[] args)
     {
       LogWithSender(LogLevel.Debug, message, args);
+    }
+
+    public static void Debug(string sender, string message, params object[] args)
+    {
+      Log(LogLevel.Debug, sender, message, args);
     }
 
     public static void Debug<T>(string message, params object[] args)
