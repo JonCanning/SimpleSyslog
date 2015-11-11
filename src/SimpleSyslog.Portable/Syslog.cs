3099d21cf31ab0beb5f6a1002526711d4de2e36f
Jon Canning <jon.canning@gmail.com>
2015-11-11 15:12:03 +0000

    Introduce useLocalTime so UTC is default

--- src/SimpleSyslog.Portable/Syslog.cs
+++ src/SimpleSyslog.Portable/Syslog.cs
@@ -11,13 +11,15 @@ namespace SimpleSyslog
     static int facility;
     static string fixedSender;
     static string clientName;
+    private static bool useLocalTime;
     public static string MessageFormat { get; set; }
 
-    public static async Task Initialize(string hostName, int port, string clientName, int facility = 16, string sender = null)
+    public static async Task Initialize(string hostName, int port, string clientName, int facility = 16, string sender = null, bool useLocalTime = false)
     {
       Syslog.clientName = clientName;
       fixedSender = sender;
       Syslog.facility = facility;
+      Syslog.useLocalTime = useLocalTime;
       udpClient = new UdpSocketClient();
       await udpClient.ConnectAsync(hostName, port);
     }
@@ -39,122 +41,122 @@ namespace SimpleSyslog
       await Log(logLevel, sender, message, args);
     }
 
-    public async static Task Emergency(string sender, object arg)
+    public static async Task Emergency(string sender, object arg)
     {
       await Emergency(sender, arg.ToString());
     }
 
-    public async static Task Emergency(string sender, string message, params object[] args)
+    public static async Task Emergency(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Emergency, message, args);
     }
 
-    public async static Task Emergency<T>(string message, params object[] args) where T : class
+    public static async Task Emergency<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Emergency, message, args);
     }
 
-    public async static Task Alert(string sender, object arg)
+    public static async Task Alert(string sender, object arg)
     {
       await Alert(sender, arg.ToString());
     }
 
-    public async static Task Alert(string sender, string message, params object[] args)
+    public static async Task Alert(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Alert, message, args);
     }
 
-    public async static Task Alert<T>(string message, params object[] args) where T : class
+    public static async Task Alert<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Alert, message, args);
     }
 
-    public async static Task Critical(string sender, object arg)
+    public static async Task Critical(string sender, object arg)
     {
       await Critical(sender, arg.ToString());
     }
 
-    public async static Task Critical(string sender, string message, params object[] args)
+    public static async Task Critical(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Critical, message, args);
     }
 
-    public async static Task Critical<T>(string message, params object[] args) where T : class
+    public static async Task Critical<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Critical, message, args);
     }
 
-    public async static Task Error(string sender, object arg)
+    public static async Task Error(string sender, object arg)
     {
       await Error(sender, arg.ToString());
     }
 
-    public async static Task Error(string sender, string message, params object[] args)
+    public static async Task Error(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Error, message, args);
     }
 
-    public async static Task Error<T>(string message, params object[] args) where T : class
+    public static async Task Error<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Error, message, args);
     }
 
-    public async static Task Warn(string sender, object arg)
+    public static async Task Warn(string sender, object arg)
     {
       await Warn(sender, arg.ToString());
     }
 
-    public async static Task Warn(string sender, string message, params object[] args)
+    public static async Task Warn(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Warn, message, args);
     }
 
-    public async static Task Warn<T>(string message, params object[] args) where T : class
+    public static async Task Warn<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Warn, message, args);
     }
 
-    public async static Task Notice(string sender, object arg)
+    public static async Task Notice(string sender, object arg)
     {
       await Notice(sender, arg.ToString());
     }
 
-    public async static Task Notice(string sender, string message, params object[] args)
+    public static async Task Notice(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Notice, message, args);
     }
 
-    public async static Task Notice<T>(string message, params object[] args) where T : class
+    public static async Task Notice<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Notice, message, args);
     }
 
-    public async static Task Info(string sender, object arg)
+    public static async Task Info(string sender, object arg)
     {
       await Info(sender, arg.ToString());
     }
 
-    public async static Task Info(string sender, string message, params object[] args)
+    public static async Task Info(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Info, message, args);
     }
 
-    public async static Task Info<T>(string message, params object[] args) where T : class
+    public static async Task Info<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Info, message, args);
     }
 
-    public async static Task Debug(string sender, object arg)
+    public static async Task Debug(string sender, object arg)
     {
       await Debug(sender, arg.ToString());
     }
 
-    public async static Task Debug(string sender, string message, params object[] args)
+    public static async Task Debug(string sender, string message, params object[] args)
     {
       await LogWithSender(sender, LogLevel.Debug, message, args);
     }
 
-    public async static Task Debug<T>(string message, params object[] args) where T : class
+    public static async Task Debug<T>(string message, params object[] args) where T : class
     {
       await Log<T>(LogLevel.Debug, message, args);
     }
@@ -165,7 +167,8 @@ namespace SimpleSyslog
         return;
       var messageFormat = MessageFormat ?? "{message}";
       message = messageFormat.Replace("{message}", message);
-      message = string.Format("<{0}>{1} {2} {3} {4}", facility * 8 + (int)logLevel, DateTime.UtcNow.ToString("s"), clientName, sender, message);
+      message =
+        $"<{facility*8 + (int) logLevel}>{(useLocalTime ? DateTime.Now.ToString("s") : DateTime.UtcNow.ToString("s"))} {clientName} {sender} {message}";
       var bytes = Encoding.UTF8.GetBytes(message);
       await udpClient.SendAsync(bytes);
     }
